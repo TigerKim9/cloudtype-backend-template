5298637a53c073e5ba0965e97b6ad85abdd5e1c0
TigerKim9 <kkm9499@gmail.com>
2022-12-19 23:59:49 +0900

    test

--- src/test/java/io/cloudtype/waitlist/WaitlistBackendApplicationTests.java
+++ src/test/java/io/cloudtype/waitlist/WaitlistBackendApplicationTests.java
@@ -1,5 +1,7 @@
 package io.cloudtype.waitlist;
 
+import io.cloudtype.waitlist.model.Guest;
+import io.cloudtype.waitlist.repository.GuestRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class WaitlistBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Guest guest = Guest
 	}
 
 }
