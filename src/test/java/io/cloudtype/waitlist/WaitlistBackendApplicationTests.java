027033d99ceee6a59c50c8a3d5e5521bd0378ab6
TigerKim9 <kkm9499@gmail.com>
2022-12-20 00:04:24 +0900

    JPA practice test

--- src/test/java/io/cloudtype/waitlist/WaitlistBackendApplicationTests.java
+++ src/test/java/io/cloudtype/waitlist/WaitlistBackendApplicationTests.java
@@ -1,5 +1,6 @@
 package io.cloudtype.waitlist;
 
+import io.cloudtype.waitlist.exception.ResourceNotFoundException;
 import io.cloudtype.waitlist.model.Guest;
 import io.cloudtype.waitlist.repository.GuestRepository;
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WaitlistBackendApplicationTests {
 
+	GuestRepository guestRepository;
 	@Test
-	void contextLoads() {
-		Guest guest = Guest
+	void contextLoads(Long id) {
+		Guest guest  = guestRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Guest not found."));
 	}
 
 }
