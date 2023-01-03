7424f82294646d18536f5c434a10b6c294a4220a
TigerKim9 <kkm9499@gmail.com>
2023-01-03 23:45:46 +0900

    test code fix

--- src/test/java/io/cloudtype/waitlist/WaitlistBackendApplicationTests.java
+++ src/test/java/io/cloudtype/waitlist/WaitlistBackendApplicationTests.java
@@ -13,10 +13,7 @@ class WaitlistBackendApplicationTests {
 	@Test
 	void contextLoads(Long id) {
 		Guest guest  = guestRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Guest not found."));
-		//컴퓨터 변화 테스트
-		assert();
-		//git 반영 테스트 문제 발생..
-		assert;
+		
 	}
 
 }
